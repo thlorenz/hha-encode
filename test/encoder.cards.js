8429a8f3deb3ea6fc596d0011de46a2765fd1684
Thorsten Lorenz <thlorenz@gmx.de>
2017-11-15 23:58:12 +0100

    code: renaming translators to encoders
    - since that's what they are and that name is more common

--- test/encoder.cards.js
+++ test/encoder.cards.js
@@ -7,13 +7,13 @@ const ocat = require('./util/ocat')
 // eslint-disable-next-line no-unused-vars
 const { bin } = require('../lib/util')
 
-const CardsTranslator = require('../lib/translator.cards')
+const CardsEncoder = require('../lib/encoder.cards')
 
 function roundtrip(t, original) {
   const len = original.length
-  const translator = new CardsTranslator([ 0 ], len)
-  const encoded = translator.encode(original)
-  const decoded = translator.decode(encoded)
+  const encoder = new CardsEncoder([ 0 ], len)
+  const encoded = encoder.encode(original)
+  const decoded = encoder.decode(encoded)
   original.$topic = `${len} cards`
   spok(t, decoded, original)
 }
@@ -31,9 +31,9 @@ test('\npassing null or other invalid card i.e. it was not present', function(t)
   const original = [ 'Ks', null, 'N/A', '?', '??' ]
   const expected = [ 'Ks', '??', '??', '??', '??' ]
 
-  const translator = new CardsTranslator([ 0 ], original.length)
-  const encoded = translator.encode(original)
-  const decoded = translator.decode(encoded)
+  const encoder = new CardsEncoder([ 0 ], original.length)
+  const encoded = encoder.encode(original)
+  const decoded = encoder.decode(encoded)
 
   spok(t, decoded, expected)
   t.end()
@@ -43,10 +43,10 @@ test('\ntwo cards in center of 24 bytes', function(t) {
   //          |23-18|17-12| 11-6| 5-0 |
   var bits = 0b000000000000000000000000
   const original = [ 'As', 'Kd' ]
-  const translator = new CardsTranslator([ 6 ], 2)
-  bits |= translator.encode(original)
+  const encoder = new CardsEncoder([ 6 ], 2)
+  bits |= encoder.encode(original)
   t.equal(bits, 0b000000000110000011000000, 'encodes card in the center')
-  const decoded = translator.decode(bits)
+  const decoded = encoder.decode(bits)
   spok(t, decoded, original)
   t.end()
 })
