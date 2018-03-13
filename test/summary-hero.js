f2dac2b48006d242efe4ca7c8ffb4d9f29021880
Thorsten Lorenz <thlorenz@gmx.de>
2018-03-12 21:16:15 -0600

    hero: fixing encoding/decoding when no hero is present

--- test/summary-hero.js
+++ test/summary-hero.js
@@ -8,6 +8,7 @@ const { encode, decode } = require('../lib/summary/hero')
 function roundtrip(t, original) {
   const encoded = encode(original)
   const decoded = decode(encoded)
+  if (original == null && decoded == null) return t.pass('handles null hero')
   spok(t, decoded, original)
 }
 
@@ -15,5 +16,8 @@ test('\nsummary:hero: roundtripping heros', function(t) {
   roundtrip(t, { pos: 'UTG+1', stackRatio: 22.4, cards: { card1: '2s', card2: '4c' } })
   roundtrip(t, { pos: 'SB', stackRatio: 212.0, cards: { card1: 'Ad', card2: 'As' } })
   roundtrip(t, { pos: 'LJ', stackRatio: 0.2, cards: { card1: '5h', card2: '7d' } })
+
+  // no hero
+  roundtrip(t, null)
   t.end()
 })
